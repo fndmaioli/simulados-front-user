b3e417e0d4df18ca4f6bd3b0c45dda4dbd7517da
Gregory Lagranha <gregory_lagranha@hotmail.com>
2018-10-03 19:15:57 -0300

    Result Layout

--- src/scenes/Result/index.js
+++ src/scenes/Result/index.js
@@ -23,28 +23,50 @@ import Slider from 'react-slick'
 class Result extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { showConfirmButton: false }
+    this.state = { showConfirmButton: false, hits: 60, total: 80 }
+  }
+
+  getMessage() {
+    let score = this.state.total / 2
+    if (this.state.hits > score) return 'Acima da média! :)'
+    if (this.state.hits == score) return 'Na média! :|'
+    if (this.state.hits < score) return 'Abaixo da média :('
+  }
+
+  getClass() {
+    let score = this.state.total / 2
+    if (this.state.hits > score) return 'green'
+    if (this.state.hits == score) return 'yellow'
+    if (this.state.hits < score) return 'red'
   }
 
   render() {
-    const settings = {
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-    }
-
     return (
       <Container>
-        <Card>
-          <h1>Resultado</h1>
-          <h3>30/40</h3>
-          <Card>Porcentagem de acertos 75%</Card>
-          <Card>Tempo médio por questão 2m20s</Card>
-          <Card>Tempo total 3h30s</Card>
-        </Card>
+        <h1 className={this.getClass()}>
+          {this.state.hits}/{this.state.total}
+        </h1>
+        <h3>{this.getMessage()}</h3>
+
+        <label className="data-result">
+          Porcentagem de acertos <span>75%</span>
+        </label>
+        <label className="data-result">
+          Tempo médio por questão <span>2m20s</span>
+        </label>
+        <label className="data-result">
+          Tempo total <span>3h30s</span>
+        </label>
+
+        <br />
+        <br />
+        <br />
+
+        <h3>Questões</h3>
+
+        <Card>Porcentagem de acertos 75%</Card>
+        <Card>Tempo médio por questão 2m20s</Card>
+        <Card>Tempo total 3h30s</Card>
       </Container>
     )
   }
