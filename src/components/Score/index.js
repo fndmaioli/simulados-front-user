8437b1ef50e11c0e9f771b7ba51e356e9fd0bf49
Gregory Lagranha <gregory_lagranha@hotmail.com>
2018-10-09 19:38:41 -0300

    component Score created / remove

---
+++ src/components/Score/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import './score.scss'
+
+class Score extends React.Component {
+  getMessage(hits, total) {
+    let score = total / 2
+    if (hits > score) return 'Acima da média! :)'
+    if (hits == score) return 'Na média! :|'
+    if (hits < score) return 'Abaixo da média :('
+  }
+
+  getClass(hits, total) {
+    let score = total / 2
+    if (hits > score) return 'score-green'
+    if (hits == score) return 'score-yellow'
+    if (hits < score) return 'score-red'
+  }
+
+  render() {
+    const { title, hits, total } = this.props
+
+    const Elem = title ? 'h1' : 'strong'
+    const SubTitle = () =>
+      title ? <h3>{this.getMessage(hits, total)}</h3> : ''
+
+    return (
+      <div className="score">
+        <Elem className={this.getClass(hits, total)}>
+          {hits}/{total}
+        </Elem>
+        <SubTitle />
+      </div>
+    )
+  }
+}
+
+export default Score
--- src/scenes/Result/index.js
+++ src/scenes/Result/index.js
@@ -4,50 +4,25 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { growl } from 'store/ui/actions'
-import { GROWL_INFO, GROWL_ERROR, GROWL_SUCCESS } from 'store/ui/constants'
 
-import Button from 'components/Button'
-import Input from 'components/Input'
-import Field from 'components/Field'
 import Card from 'components/Card'
-import RadioGroup from 'components/RadioGroup'
-import { Radio } from 'components/RadioGroup'
-import Select from 'components/Select'
-import Modal from 'components/Modal'
 import './result.scss'
 import Container from 'components/Container'
 import Icon from 'components/Icon'
 import 'slick-carousel/slick/slick.scss'
 import 'slick-carousel/slick/slick-theme.scss'
-import Slider from 'react-slick'
+import Score from 'components/Score'
 
 class Result extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { showQuestions: false, hits: 60, total: 80 }
-  }
-
-  getMessage() {
-    let score = this.state.total / 2
-    if (this.state.hits > score) return 'Acima da média! :)'
-    if (this.state.hits == score) return 'Na média! :|'
-    if (this.state.hits < score) return 'Abaixo da média :('
-  }
-
-  getClass() {
-    let score = this.state.total / 2
-    if (this.state.hits > score) return 'green'
-    if (this.state.hits == score) return 'yellow'
-    if (this.state.hits < score) return 'red'
+    this.state = { showQuestions: false }
   }
 
   render() {
     return (
       <Container>
-        <h1 className={this.getClass()}>
-          {this.state.hits}/{this.state.total}
-        </h1>
-        <h3>{this.getMessage()}</h3>
+        <Score title={true} hits={60} total={80} />
 
         <div className="data-result data-result-border-bottom">
           <div>Porcentagem de acertos</div> <div>75%</div>
@@ -66,32 +41,14 @@ class Result extends React.Component {
         <h3>Questões</h3>
 
         <Card>
-          <div className="data-result-areas">
+          <div className="data-result data-result-areas">
             <div>Direito Civil</div>
             <div>
-              <strong>6/7</strong>
+              <Score hits={6} total={7} />
+            </div>
+            <div>
               <Icon name="chevron-down" />
             </div>
-          </div>
-          <div>
-            <div>
-              Direito Civil
-              <span>
-                <Icon name="chevron-down" className="right" />
-              </span>
-            </div>
-          </div>
-        </Card>
-        <Card>
-          <div className="data-result">
-            Direito Penal
-            <Icon name="chevron-down" className="right" />
-          </div>
-        </Card>
-        <Card>
-          <div className="data-result">
-            Direito Criminal
-            <Icon name="chevron-down" className="right" />
           </div>
         </Card>
       </Container>
